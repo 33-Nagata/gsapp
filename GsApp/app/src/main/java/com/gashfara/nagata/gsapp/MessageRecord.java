bcd5d0ae1af73f13dd8ec50224fa268f5c979c9e
Shinsuke Nagata <shinsuke.ngt@gmail.com>
2016-05-21 16:40:55 +0900

    NewListView init

--- GsApp/app/src/main/java/com/gashfara/nagata/gsapp/MessageRecord.java
+++ GsApp/app/src/main/java/com/gashfara/nagata/gsapp/MessageRecord.java
@@ -3,15 +3,20 @@ package com.gashfara.nagata.gsapp;
 
 public class MessageRecord {
     //保存するデータ全てを変数で定義します。
+    private String id;
     private String imageUrl;
     private String comment;
 
     //データを１つ作成する関数です。項目が増えたら増やしましょう。
-    public MessageRecord(String imageUrl, String comment) {
+    public MessageRecord(String id, String imageUrl, String comment) {
+        this.id = id;
         this.imageUrl = imageUrl;
         this.comment = comment;
     }
     //それぞれの項目を返す関数です。項目が増えたら増やしましょう。
+    public String getId() {
+        return id;
+    }
     public String getComment() {
         return comment;
     }
