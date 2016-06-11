6d0a7f0e0441e8cdacc9446815c8033bce997025
Shinsuke Nagata <shinsuke.ngt@gmail.com>
2016-06-11 14:14:08 +0900

    good init

--- GsApp/app/src/main/java/com/gashfara/nagata/gsapp/MessageRecord.java
+++ GsApp/app/src/main/java/com/gashfara/nagata/gsapp/MessageRecord.java
@@ -6,12 +6,14 @@ public class MessageRecord {
     private String id;
     private String imageUrl;
     private String comment;
+    private int goodCount;
 
     //データを１つ作成する関数です。項目が増えたら増やしましょう。
-    public MessageRecord(String id, String imageUrl, String comment) {
+    public MessageRecord(String id, String imageUrl, String comment, int goodCount) {
         this.id = id;
         this.imageUrl = imageUrl;
         this.comment = comment;
+        this.goodCount = goodCount;
     }
     //それぞれの項目を返す関数です。項目が増えたら増やしましょう。
     public String getId() {
@@ -21,6 +23,14 @@ public class MessageRecord {
         return comment;
     }
     public String getImageUrl() {
+
         return imageUrl;
+    }
+    public int getGoodCount() {
+        return goodCount;
+    }
+    //セットする関数.項目が増えたら追加しましょう
+    public void setGoodCount(int goodCount) {
+        this.goodCount = goodCount;
     }
 }
